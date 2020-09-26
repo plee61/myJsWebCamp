21ccd1e966bf5620c764de1a0b2d9bd1a6435984
plee61 <55860578+plee61@users.noreply.github.com>
2020-09-26 11:06:04 +1000

    Chap6 Arrays Basics

---
+++ Array/todo.js
@@ -0,0 +1,7 @@
+const tolearnList = ['JavaScript', 'JQuery','.Net','Java','Phone development']
+console.log(`You have ${tolearnList.length} things to learn`)
+console.log('item 1 ~' + tolearnList[0])
+console.log('item 2 ~' + tolearnList[1])
+console.log('item 3 ~' + tolearnList[2])
+console.log('item 4 ~' + tolearnList[3])
+console.log('item 5 ~' + tolearnList[4])
