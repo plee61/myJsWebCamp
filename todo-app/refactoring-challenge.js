94ab413728d99ed78f90f5da7973dcb7f9c199d7
Hannah <55860578+plee61@users.noreply.github.com>
2020-10-24 10:54:56 +1100

    moved displayTodos to commonFunctions.js

--- todo-app/refactoring-challenge.js
+++ todo-app/refactoring-challenge.js
@@ -1,13 +1,3 @@
-const displayTodos = function(){
-    document.querySelector('#todos-div').innerHTML = ''
-    
-    // todos.forEach(function (todo) { ** chrome does not save multiple data for each key
-        const p = document.createElement('p')
-        p.textContent = JSON.parse(localStorage.getItem('todos')).title
-        document.querySelector('#todos-div').appendChild (p)
-      //})
-}    
-
 const addTodo = function (newTodo){
     const todo = JSON.stringify({title:newTodo,completed:false})
     localStorage.setItem('todos',todo)
