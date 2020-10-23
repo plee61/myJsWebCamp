ae72a0a7f9d02ffcb3a55b60978ebee61ddb2685
Hannah <55860578+plee61@users.noreply.github.com>
2020-10-24 10:52:46 +1100

    added a function displayTodos
    referenced by refactoring js

--- todo-app/commonFunctions.js
+++ todo-app/commonFunctions.js
@@ -1,1 +1,10 @@
 
+const displayTodos = function(){
+    document.querySelector('#todos-div').innerHTML = ''
+    
+    // todos.forEach(function (todo) { ** chrome does not save multiple data for each key
+        const p = document.createElement('p')
+        p.textContent = JSON.parse(localStorage.getItem('todos')).title
+        document.querySelector('#todos-div').appendChild (p)
+      //})
+}  
