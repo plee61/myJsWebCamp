01c2d1387200039afd04ffab5728cb9ab3b0e9a1
Hannah <55860578+plee61@users.noreply.github.com>
2020-10-24 12:40:02 +1100

    added checkbox completed status

--- todo-app/commonFunctions.js
+++ todo-app/commonFunctions.js
@@ -1,4 +1,12 @@
-
+const deleteCallBack(todoId){
+   const todoIndex = todos.findIndex(function(todo){
+        return todo.id = todoId
+    })
+   if (todoIndex > -1) {
+       todos.splice(todoIndex,1)
+       displayTodos()
+   }
+}
 const displayTodos = function(){
     document.querySelector('#todos-div').innerHTML = ''
     
@@ -8,13 +16,20 @@ const displayTodos = function(){
         const cb = document.createElement('input')
         const sp = document.createElement('span')
         const btn = document.createElement('btn')
+        todoStorage = JSON.parse(localStorage.getItem('todos'))
+        
         cb.setAttribute('type','checkbox')
-        sp.textContent = JSON.parse(localStorage.getItem('todos')).title
+        cb.checked = todoStorage.completed 
+    
+        sp.textContent = todoStorage.title
         btn.text = 'x'
+        
         document.querySelector('#todos-div').appendChild (d)
         document.querySelector('#todos-div').appendChild (cb)
         document.querySelector('#todos-div').appendChild (sp)
         document.querySelector('#todos-div').appendChild (btn)
+        cb.addEventListner('click', deleteCallBack(todoStorage.id))
+        
       //})
 }  
 const addTodo = function (newTodo){
